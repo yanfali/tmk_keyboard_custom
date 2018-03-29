e86f25c26be1e5c6471f8007f570acee68f73ef8
Yan-Fa Li <yanfa.li@fireeye.com>
2018-03-29 00:22:19 -0700

    Compiling but not tested
     - map macros
       It's a nested family of macros that ultimately seems to expand to
       mapping to KC_##SOMETHING

--- keyboard/xd84/keymap_default.c
+++ keyboard/xd84/keymap_default.c
@@ -8,27 +8,27 @@ const uint8_t keymaps[KEYMAPS_COUNT][MATRIX_SIZE] __attribute__ ((section (".key
 const uint8_t keymaps[][MATRIX_SIZE] PROGMEM = {
 #endif
     /* Keymap 0: Default Layer
-     * ,---------------------------------------------------------------------------------------.
-     * |Esc|   | F1| F2| F3| F4| | F5| F6| F7| F8| | F9|F10|F11|F12|Psc|Slk|Pus|               |
-     * |---------------------------------------------------------------------------------------|
-     * |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Ins|Hom|PgU|Num|  /|  *|  -|
-     * |---------------------------------------------------------------------------------------|
-     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|Del|End|PgD|  7|  8|  9|   |
-     * |-----------------------------------------------------------------------------------|  +|
-     * |Caps  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return  |           |  4|  5|  6|   |
-     * |-----------------------------------------------------------|   ,---.   |---------------|
-     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |   |Up |   |  1|  2|  3|   |
-     * |-----------------------------------------------------------------------------------|Ent|
-     * |Ctrl|Gui |Alt |         Space          |Alt |Gui |Fn0 |Ctrl|Lef|Dow|Rig|      0|  .|   |
-     * `---------------------------------------------------------------------------------------'
+     * ,-----------------------------------------------------------------.
+     * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Psc|Pus|Del  |
+     * |-----------------------------------------------------------------|
+     * |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Hom  |
+     * |-----------------------------------------------------------------|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|   \ |PgUp |
+     * |-----------------------------------------------------------------|
+     * |Caps  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return  |PgDn |
+     * |---------------------------------------------------------,---.   |
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift  |Up|End  |
+     * |-----------------------------------------------------------------|
+     * |Ctrl|Gui |Alt |         Space          |Alt |Gui |Fn0|Lf|Do|Ri   |
+     * `-----------------------------------------------------------------'
      */
-    KEYMAP_ANSI_104(
-        ESC,      F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,   PSCR,SLCK,PAUS,                       \
-        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,  INS, HOME,PGUP,  NLCK,PSLS,PAST,PMNS, \
-        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,  DEL, END, PGDN,  P7,  P8,  P9,  PPLS, \
-        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,                    P4,  P5,  P6,        \
-        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT,       UP,         P1,  P2,  P3,  PENT, \
-        LCTL,LGUI,LALT,               SPC,                RALT,RGUI,FN0, RCTL,  LEFT,DOWN,RGHT,  P0,       PDOT       ),
+    KEYMAP_ANSI_XD84(
+        ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, PSCR,PAUS, DEL,  \
+        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,      HOME, \
+        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,      PGUP, \
+        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,       PGDN, \
+        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,    RSFT,       UP,  END,  \
+        LCTL,LGUI,LALT,               SPC,                RALT,RGUI,FN0, LEFT, DOWN,RGHT),
 };
 
 /*
